4a7c99df586d5120b9ac78787f8b13004a99f76f
MitsruYakushinji <156178833+MitsruYakushinji@users.noreply.github.com>
2024-04-16 20:04:47 +0900

    theme追加

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from './color'
+import fontSizes from './fontSize'
+import letterSpacings from './letterSpacings'
+import lineHeight from './lineHeight'
+import space from './space'
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeight,
+  colors,
+} as const
---
+++ src/themes/space.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const space: any = ['0px', '8px', '16px', '32px', '64px']
+
+// aliases
+space.small = space[0]
+space.medium = space[1]
+space.Large = space[2]
+
+export default space
